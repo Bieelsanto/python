fb13616dc1113605cc6336c389880448c5514d94
Biel Santos <59985885+Bieelsanto@users.noreply.github.com>
2023-04-02 10:03:05 -0300

    Exercícios

---
+++ execercios/exe02.py
@@ -0,0 +1,5 @@
+print('================')
+print('Qual o seu nome?')
+nome = input('Nome: ')
+print('Prazer te conhecer, {}!'.format(nome))
+print('=================')
---
+++ execercios/exe03.py
@@ -0,0 +1,6 @@
+print('=======================')
+print('Diga números aleatórios')
+num1 = int(input('Primeiro Número: '))
+num2 = int(input('Segundo Número: '))
+res = num1 + num2
+print('O resultado da soma entre {} e {} é {}'.format(num1, num2, res))
