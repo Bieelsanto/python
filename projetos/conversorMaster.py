c6fde9a4133f96d8507355ac52aa4083e886c228
Biel Santos <59985885+Bieelsanto@users.noreply.github.com>
2023-04-02 10:02:55 -0300

    Progresso ENIGMA

--- projetos/conversorMaster.py
+++
@@ -1,2 +0,0 @@
-""" Hoje iremos tentar fazer um conversor de bases universal em Python """
-""" Sou um programador amador, comecei do absoluto 0 faz apenas uns 3 meses. O objetivo é puramente educativo :) """
